e6f475b3226317946f70c5e8a5fe0ca002cffa36
Sridhar <sridharmasthan@gmail.com>
2026-02-24 01:26:47 +0530

    fix: Fix int_orders_enriched.sql file

--- models/intermediate/int_orders_enriched.sql
+++ models/intermediate/int_orders_enriched.sql
@@ -6,6 +6,6 @@ SELECT
     c.CUSTOMER_CITY,
     c.CUSTOMER_STATE,
     o.ORDER_PURCHASE_DATE
-FROM staging.stg_order o
-JOIN staging.stg_customer c
+FROM {{ ref('stg_order') }} o
+JOIN {{ ref('stg_customer') }} c
 ON o.CUSTOMER_ID = c.CUSTOMER_ID
